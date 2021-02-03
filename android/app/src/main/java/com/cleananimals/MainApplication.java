f37293a5c2c34eabc1e2218b52817cfdfafaf750
Daniel Kiesshau <danielkiesshau@outlook.com>
2021-02-03 13:48:25 -0300

    link rnp

--- android/app/src/main/java/com/cleananimals/MainApplication.java
+++ android/app/src/main/java/com/cleananimals/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.microsoft.codepush.react.CodePush;
 import com.facebook.react.ReactInstanceManager;
